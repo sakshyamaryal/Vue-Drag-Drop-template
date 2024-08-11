32a68c71f07534b03b528cded113aa812d2fc320
sakshyamaryal <sakshyamaryal1@gmail.com>
2024-08-11 13:43:31 +0545

    deploy git

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -14,5 +12,8 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  base: process.env.NODE_ENV === 'production'
+    ? '/Vue-Drag-Drop-template/' // Replace with your repository name if necessary
+    : '/'
 })
